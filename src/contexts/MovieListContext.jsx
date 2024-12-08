256f646362e13bf1d87d0e053bad091d5fb8ab74
bong-suk <greedi9468@hanmail.net>
2024-12-08 19:52:24 +0900

    misson2

--- src/contexts/MovieListContext.jsx
+++ src/contexts/MovieListContext.jsx
@@ -1,10 +1,30 @@
-import { createContext, useState } from "react";
-import movieListData from "../../data/movieListData.json";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const MovieListContext = createContext();
 
 const MovieListProvider = ({ children }) => {
-  const [movieList, setMovieList] = useState(movieListData.results);
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchMovieList = async () => {
+      setIsLoading(true);
+      try {
+        const apiKey = import.meta.env.VITE_API_KEY;
+        const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=ko-KR`;
+        const response = await axios.get(apiUrl);
+        setMovieList(response.data.results);
+      } catch (error) {
+        console.error("영화 목록을 가져오는 중 오류 발생:", error);
+        setError("영화 목록을 가져오는 데 실패했습니다.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovieList();
+  }, []);
 
   return (
     <MovieListContext.Provider value={{ movieList, setMovieList }}>
