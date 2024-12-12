46b1c1dbfc8b6ef065d74d2fa01847b211e365f1
bong-suk <greedi9468@hanmail.net>
2024-12-12 15:33:12 +0900

    vercel error 처리

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // 추가된 부분
+  build: {
+    outDir: "dist", // 빌드 출력 디렉토리
+    assetsDir: "assets", // 에셋 디렉토리
+    sourcemap: false, // 소스맵 비활성화 (선택사항)
+  },
+});
