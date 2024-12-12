ad5f15daab4e0dffbd3fc346bf4cc8dd4f084dcc
bong-suk <greedi9468@hanmail.net>
2024-12-12 15:59:04 +0900

    vercel 오류 대소문자파일 확인

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar.jsx";
+import { Navbar } from "./Navbar.jsx";
 import "./Layout.css";
 
 const Layout = () => {
