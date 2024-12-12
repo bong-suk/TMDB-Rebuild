ea2adcc2081cb8c052b1e62bb9138535dec6edbb
bong-suk <greedi9468@hanmail.net>
2024-12-12 15:44:39 +0900

    vercel 오류 대소문자파일 확인

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import NavBar from "./Navbar.jsx"; // 수정된 부분
+import Navbar from "./Navbar.jsx"; // Navbar.jsx로 수정 (파일 이름과 정확히 일치)
 import "./Layout.css";
 
 const Layout = () => {
   return (
     <div className="layout">
-      <NavBar /> {/* 수정된 부분 */}
+      <Navbar /> {/* 컴포넌트 사용시에도 Navbar로 수정 */}
       <main className="main-content">
         <div className="container">
           <Outlet />
