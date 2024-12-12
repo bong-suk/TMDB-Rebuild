5ba685020821879b527571ec744d6b67f957083b
bong-suk <greedi9468@hanmail.net>
2024-12-12 15:10:58 +0900

    misson5

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,6 +8,7 @@ import { getMovieList } from "./axios";
 import SearchResults from "./components/Search";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import MyPage from "./pages/MyPage";
 
 const AppRoutes = () => {
   const [movieList, setMovieList] = useState([]);
@@ -50,6 +51,7 @@ const AppRoutes = () => {
         <Route path="/search" element={<SearchResults />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Route>
     </Routes>
   );
